6c342307b4b0ed7eadc4aaf84e7a245785ba911b
sinannar <sinan.nar@gmail.com>
2014-11-21 14:56:37 +0200

    generateDataset
    commented un used code cleaned

--- app/src/main/java/com/furkantektas/braingames/data/FindOperationAdapter.java
+++ app/src/main/java/com/furkantektas/braingames/data/FindOperationAdapter.java
@@ -214,23 +214,7 @@ public class FindOperationAdapter  extends BaseAdapter{
 
         for(int i = 0; i < size; ++i) {
             MathOperation c;
-
-            //int num1,num2;
             int rand1 = r.nextInt(MathOperation.Operation.values().length);
-/*
-            do {
-                num1 = r.nextInt(10);
-            }while(num1 == 0 || num1 == 1);
-
-            if(rand1 == 2 || rand1 == 3){
-                do {
-                    num2 = r.nextInt(10);
-                }while(num2 == 0 || num2 == 1);
-            }
-            else
-                num2 = r.nextInt(100);
-            c = new MathOperation(MathOperation.generateOperation(rand1),num1,num2);
-*/
             c = new MathOperation(MathOperation.generateOperation(rand1));
             mDataSet.add(c);
         }
