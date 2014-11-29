fa0daa8e7fa679c3d0075aaba346331940d0fbb8
sinannar <sinan.nar@gmail.com>
2014-11-29 02:45:33 +0200

    deleting unnecessary imports

--- app/src/main/java/com/furkantektas/braingames/ui/GraphActivity.java
+++ app/src/main/java/com/furkantektas/braingames/ui/GraphActivity.java
@@ -2,13 +2,6 @@ package com.furkantektas.braingames.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.jjoe64.graphview.GraphView;
